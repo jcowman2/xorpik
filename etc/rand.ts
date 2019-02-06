03a43721e16dfcd91366a26be95c7bc133ac38db
Joe Cowman <joe.cowman@huskers.unl.edu>
2019-02-05 22:48:56 -0600

    Experimenting using regal

---
+++ etc/rand.ts
@@ -0,0 +1,18 @@
+import { Charsets, Game, onStartCommand } from "regal";
+
+onStartCommand(game => {
+    game.output.write(
+        game.random.string(96, Charsets.ALHPANUMERIC_CHARSET + "+/")
+    );
+});
+
+Game.init({ name: "rand", author: "lars" });
+
+// tslint:disable-next-line:no-console
+console.log(
+    "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+P+/HgAFhAJ/wlseKgAAAABJRU5ErkJggg=="
+        .length
+);
+
+// tslint:disable-next-line:no-console
+console.log(Game.postStartCommand().output.log[0].data);
--- test/project.test.ts
+++ test/project.test.ts
@@ -2,6 +2,8 @@ import { expect } from "chai";
 import "mocha";
 
 import HelloWorld from "../src/index";
+import "../etc/rand";
+import { Game } from "../etc/node_modules/regal";
 
 // TODO: Write new tests
 
@@ -9,4 +11,9 @@ describe("Project", function() {
     it("Prints Hello, World!", function() {
         expect(HelloWorld("World")).to.equal("Hello, World!");
     });
+
+    // it("etc", () => {
+    //     Game.init({ name: "test", author: "joe" });
+    //     const r = Game.postStartCommand();
+    // });
 });
